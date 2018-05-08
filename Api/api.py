e9045c39f9f95e8ba0748873203f0979c2a05999
Artur <kawala.artek@gmail.com>
2018-05-08 22:34:48 +0200

    Fixed naming conventions. Fixed some other issues in js and html files.

--- Api/api.py
+++ Api/api.py
@@ -3,11 +3,12 @@ import db
 
 app = FlaskAPI(__name__)
 
-@app.route('/example/', methods = ['GET'])
-def example():
-    return {'Hello, ' : 'world!'}
-    
-@app.route('/shop/', methods = ['GET'])
-def getShop():
-    return db.execute("SELECT * FROM pite.shop WHERE shop.city='WARSZAWA'")
 
+@app.route('/example/', methods=['GET'])
+def example():
+    return {'Hello, ': 'world!'}
+
+
+@app.route('/shop/', methods=['GET'])
+def get_shop():
+    return db.execute("SELECT * FROM pite.shop WHERE shop.city='WARSZAWA'")

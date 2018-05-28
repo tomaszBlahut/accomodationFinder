fac79f0235f935e5741aac114348052f72d47d56
palcowskapaula@gmail.com <paulaa12327>
2018-05-28 20:23:22 +0200

    #11 - simple connecting to database using dataset

--- Api/api.py
+++ Api/api.py
@@ -1,6 +1,6 @@
 from flask_api import FlaskAPI
 from flask_cors import CORS
-import db
+import dataset
 
 app = FlaskAPI(__name__)
 CORS(app)
@@ -13,4 +13,5 @@ def example():
 
 @app.route('/shop/', methods=['GET'])
 def get_shop():
-    return db.execute("SELECT * FROM pite.shop WHERE shop.city='WARSZAWA'")
+    with dataset.connect('postgresql://postgres:admin@localhost/postgres', 'pite') as database:
+        return database['shop'].columns

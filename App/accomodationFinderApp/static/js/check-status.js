a2904836cb6910f148c3872b708ca2ccba97209e
olgadobrzanska <olgadobrzanska@gmail.com>
2018-05-30 12:01:33 +0200

    #8 - change check-status.s file and remove set-id.js file

--- App/accomodationFinderApp/static/js/check-status.js
+++ App/accomodationFinderApp/static/js/check-status.js
@@ -1,22 +1,49 @@
-setInterval(show_my_table, 10000);
+function chech_status() {
 
-function show_my_table() {
-    $ajax({url: "http://127.0.0.1:5000/search",
-    type:'html',
-    success: function (FindingResult.Status)
-        {
-            //how to get status?
-            if (FindingResult.Status == "Processing") {
-                setInterval(show_my_table, 10000);
+
+    setTimeout(show_my_table, 10000);
+
+// function show_my_table() {
+//     $ajax({url: "http://127.0.0.1:5000/search",
+//     type:'html',
+//     success: function (FindingResult.Status)
+//         {
+//             //how to get status?
+//             if (FindingResult.Status == "Processing") {
+//                 setInterval(show_my_table, 10000);
+//             }
+//             else if (FindingResult.Status == "Completed") {
+//                 $('#search').html(FindingResult.Status).show();
+//             }
+//             else (FindingResult.Status == "Failed")
+//             {
+//                 alert("Something goes wrong...");
+//             }
+//         }
+//     })
+// }
+
+    //for testing response
+    var response = "Complete";
+
+    function show_my_table() {
+        $ajax({
+            url: "http://127.0.0.1:5000/search",
+            type: 'html',
+            success: function (response) {
+                $('#search').html(response).hide();
+                //how to get status?
+                if (response === "Processing" || response === "New") {
+                    setTimeout(show_my_table, 10000);
+                }
+                else if (response === "Completed") {
+                    $('#search').html(response).show();
+                }
+                else (response === "Failed")
+                {
+                    alert("Something goes wrong...");
+                }
             }
-            else if (FindingResult.Status == "Completed") {
-                $('#search').html(FindingResult.Status).show();
-            }
-            else (FindingResult.Status == "Failed")
-            {
-                alert("Something goes wrong...");
-            }
-        }
-    })
+        })
+    }
 }
-
--- App/accomodationFinderApp/static/js/set-id.js
+++
@@ -1,4 +0,0 @@
-function set_id(){
-    var x = document.getElementById("UUID").value;
-    document.getElementById("set_id").innerHTML = x;
-}

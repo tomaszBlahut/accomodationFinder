c37228a072618f782d6f20f5b8e1e514fe7a35a1
Tomasz Błahut <rzuf22@gmail.com>
2018-06-02 00:48:32 +0200

    #8 - binded changing location via search box to setting main marker to center of found location

--- App/accomodationFinderApp/static/js/google-api.js
+++ App/accomodationFinderApp/static/js/google-api.js
@@ -7,6 +7,30 @@ function init() {
     zoom: 10
   });
 
+  var marker = new google.maps.Marker({
+    map: map,
+    position: new google.maps.LatLng(50.049683, 19.944544),
+    title: 'Search center',
+    draggable: true
+  });
+  google.maps.event.addListener(marker, 'dragend', function () {
+    const position = marker.getPosition();  
+    $("#latitude").val(position.lat());
+    $("#longitude").val(position.lng());
+  });
+
+  var circle = new google.maps.Circle({
+    map: map,
+    radius: 10000,
+    fillColor: '#AA0000',
+    editable: true
+  });
+  circle.bindTo('center', marker, 'position');
+  google.maps.event.addListener(circle, 'radius_changed', function() {
+    $("#radiusInput").val(circle.getRadius());    
+  });
+
+  var inputElement = $("#pac-input");
   var searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(document.getElementById('pac-input'));
   google.maps.event.addListener(searchBox, 'places_changed', function () {
@@ -21,32 +45,12 @@ function init() {
         bounds.extend(place.geometry.location);
       }(place));
     }
+
+    marker.setPosition(bounds.getCenter());
+
     map.fitBounds(bounds);
     searchBox.set('map', map);
     map.setZoom(Math.min(map.getZoom(), 12));
-  });
-
-  var marker = new google.maps.Marker({
-    map: map,
-    position: new google.maps.LatLng(50.049683, 19.944544),
-    title: 'Some location',
-    draggable: true
-  });
-  google.maps.event.addListener(marker, 'dragend', function () {
-    const position = marker.getPosition();  
-    $("#latitude").val(position.lat());
-    $("#longitude").val(position.lng());
-  });
-
-  var circle = new google.maps.Circle({
-    map: map,
-    radius: 10000,    // 10 miles in metres
-    fillColor: '#AA0000',
-    editable: true
-  });
-  circle.bindTo('center', marker, 'position');
-  google.maps.event.addListener(circle, 'radius_changed', function() {
-    $("#radiusInput").val(circle.getRadius());    
   });
 }
 google.maps.event.addDomListener(window, 'load', init);
